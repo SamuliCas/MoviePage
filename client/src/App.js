187805f75cc1f17666aada89312e1fb57af2f303
SamuliCas <samuli.castren@gmail.com>
2023-12-12 14:58:56 +0200

    Added Login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,171 @@
 import React, { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, NavDropdown, Container, Form, Button } from 'react-bootstrap';
 
 function App() {
-
+  //State to store data fethced from backend
   const [backendData, setBackendData] = useState([{}])
 
-  useEffect (() => {
-    fetch("/api").then(
-      reponse => reponse.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
+  // States to handle username input and password input
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loginUsername, setLoginUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  
+  //Fetch data from the backend
+  useEffect(() => {
+    fetch("/users")
+      .then(response => response.json())
+      .then(data => {
+        setBackendData(data);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  // Handle the registration when the "Register" button is clicked
+  const handleLogin = () => {
+    fetch("/addUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error("Error adding user:", error);
+      });
+      // Reload the page after registration
+      window.location.reload();
+  };
+
+  const handleLoginSubmit = () => {   
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: loginUsername, password: loginPassword }),
+    })
+      .then(response => {
+        if (response.ok) {
+          // Login successful
+          console.log("Login successful");
+          setLoggedInUser(loginUsername)
+          // Add logic to redirect or handle successful login
+        } else {
+          // Login failed
+          console.error("Login failed");
+          // Add logic to display an error message or handle failed login
+        }
+      })
+      .catch(error => {
+        console.error("Error logging in:", error);
+      });
+};
 
   return (
     <div>
-      
-    {(typeof backendData.users === 'undefined') ? (
-      <p>Loading...</p>
-    ): (
-      backendData.users.map((user, i) => (
-        <p key={i}>{user}</p>
-      ))
-    )}
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>My App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action1">Action 1</NavDropdown.Item>
+                <NavDropdown.Item href="#action2">Action 2</NavDropdown.Item>
+                <NavDropdown.Item href="#action3">About</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            {loggedInUser ? (
+              <Nav>
+                <Nav.Item>
+                  <Nav.Link disabled>{loggedInUser}</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            ) : (
+              <Form inline={true.toString()}>
+                {/* Your login form goes here */}
+                {/* ... */}
+              </Form>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container>
+        <Form>
+          <Form.Group controlId="formUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button variant="primary" onClick={handleLogin}>
+            Register
+          </Button>
+        </Form>
+      </Container>
+
+      <Container>
+        <Form>
+          <Form.Group controlId="formLoginUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter username"
+              value={loginUsername}
+              onChange={(e) => setLoginUsername(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formLoginPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={loginPassword}
+              onChange={(e) => setLoginPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button variant="primary" onClick={handleLoginSubmit}>
+            Login
+          </Button>
+        </Form>
+      </Container>
+
+      {/* Just simple way to display backend data */}
+      {backendData.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.map((user, i) => (
+          <p key={i}>{user.username}</p>
+        ))
+      )}
 
     </div>
   )
--- server/server.js
+++ server/server.js
@@ -1,8 +1,77 @@
 const express = require('express')
-const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
 
-app.get("/api", (req, res) => {
-    res.json({ "users": ["userOne", "userTwo", "userThree"] })
+const app = express()
+app.use(cors())
+app.use(express.json()); 
+
+// Create a MySQL database connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: 'root',
+    password: '',
+    database: 'mydb'
 })
 
-app.listen(5000, () => {console.log("Server started on port 5000") })
+// Define a route to fetch all users from the database
+app.get('/users', (req, res)=> {
+    const sql = "SELECT * FROM users";
+    db.query(sql, (err, data)=> {
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+// Define a route to add a new user to the database
+app.post('/addUser', (req, res) => {
+    const { username, password } = req.body;
+
+    // Validate that both username and password are provided  
+    if (!username || !password) {
+      return res.status(400).json({ error: "Username and password are required." });
+    }
+  
+    const sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    db.query(sql, [username, password], (err, result) => {
+      if (err) {
+        console.error("Error adding user to the database:", err);
+        return res.status(500).json({ error: "Internal server error." });
+      }
+      
+      // Respond with an empty JSON object (success)  
+      res.json({});
+    });
+  });
+
+  app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+  
+    // Validate that both username and password are provided  
+    if (!username || !password) {
+      return res.status(400).json({ error: "Username and password are required." });
+    }
+  
+    // Check if the provided username and password match a user in the database
+    const sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+    db.query(sql, [username, password], (err, result) => {
+      if (err) {
+        console.error("Error checking login credentials:", err);
+        return res.status(500).json({ error: "Internal server error." });
+      }
+  
+      if (result.length > 0) {
+        // Login successful
+        res.json({ success: true });
+      } else {
+        // Login failed
+        res.status(401).json({ error: "Invalid username or password." });
+      }
+    });
+  });
+  
+// app.get("/api", (req, res) =>{
+//     res.json({"users": ["userOne", "userTwo", "userThree"] })
+// })
+
+app.listen(5000, () => { console.log("Server started on port 5000") })
